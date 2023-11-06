8f8cafc05ff83d7a49d0f3ca62635cf4f7494440
ckckckcz <rriovld@gmail.com>
2023-11-06 14:07:06 +0700

    Praktikum Dasar Pemrogaman

--- Praktek Daspro/Semester 1/Meet 11/Tugas3.java
+++ Praktek Daspro/Semester 1/Meet 11/Tugas3.java
@@ -1,24 +1,27 @@
 import java.util.Scanner;
 
 public class Tugas3 {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Masukkan nilai N (minimal 3): ");
+        System.out.print("Masukkan Nilai N = ");
         int N = sc.nextInt();
 
-        if (N < 3) {
-            System.out.println("Nilai N harus minimal 3.");
-        } else {
-            for (int i = 1; i <= N; i++) {
-                for (int j = 1; j <= N; j++) {
-                    if (i == 1 || i == N || j == 1 || j == N || i % 02 == 0) {
-                        System.out.print("5 ");
-                    } else {
-                        System.out.print("  ");
-                    }
+        // Perulangan untuk mencetak baris
+        for (int i = 1; i <= N; i++) {
+
+            // Perulangan untuk mencetak angka dalam satu baris
+            for (int j = 1; j <= N; j++) {
+                if (i == 1 || i == N || j == 1 || j == N) {
+                    System.out.print(N);
+                } else {
+                    System.out.print(" ");
                 }
-                System.out.println();
+                System.out.print(" "); // Menambahkan spasi antara angka
             }
+
+            // Baris baru setelah setiap baris angka
+            System.out.println();
         }
     }
 }
